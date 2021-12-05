1bd412087d8f7dd1b176f4bdde53031c97e8b095
Dan Christian <dansilog16@gmail.com>
2021-12-05 20:38:05 +0545

    intro

--- guess.py
+++ guess.py
@@ -4,3 +4,11 @@
 # Display “Greater than” if the inputed number is greater than the random number
 # Display “Less than” if the inputed number is less than the random number
 # Repeat asking the user until the random number has been guessed correctly.
+
+# introduction
+print("\n\033[1;37;45mWelcome!\033[0m\n")
+# ask for user
+user = input("\nPlease enter your \033[1mname\033[0m: ")
+# welcome
+print(f"\n\033[7;34;40mWelcome {user}!, this program is a guess game!\033[0m \n")
+
